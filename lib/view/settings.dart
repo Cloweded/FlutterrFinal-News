d98ccff5da46d613379127f808a29647ceffd11b
Ömer Yiğit Kasap <138334472+Cloweded@users.noreply.github.com>
2024-06-10 01:19:02 +0300

    Update settings.dart

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -32,8 +32,8 @@ class _SettingsState extends State<Settings> {
               themeIsLight = !themeIsLight;
             });
           },
-          title: Text("Tema"),
-          trailing: Text(themeIsLight?"Açık" : "Koyu"),
+          title: Text("Temaa"),
+          trailing: Text(themeIsLight?"Açık" : "Koyuu"),
         ),
           Divider(),
 
